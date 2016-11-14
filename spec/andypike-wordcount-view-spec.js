254c323e1a68fb4bfc00c917da862f8f429f6565
Andy Pike <andy@andypike.com>
2016-11-14 23:37:39 +0000

    Initial Project

---
+++ spec/andypike-wordcount-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AndypikeWordcountView from '../lib/andypike-wordcount-view';
+
+describe('AndypikeWordcountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
